d1fb7f07dbf617d8a077a80a637975b9d8e3d589
Harsha Vardhan <80092815+harshav167@users.noreply.github.com>
2024-05-24 18:33:34 +1000

    fix sonarqube not working atm

--- frontend/locustfile.py
+++ frontend/locustfile.py
@@ -1,11 +1,12 @@
 from locust import HttpUser, task, between
 
+
 class ShoppingCartUser(HttpUser):
     wait_time = between(1, 3)  # Users wait between 1 and 3 seconds between tasks
-      
+
     def on_start(self):
         self.client.headers = {
-            'Accept-Language': 'en-US',  # Always request the English (US) locale
+            "Accept-Language": "en-US",  # Always request the English (US) locale
         }
 
     @task
@@ -20,7 +21,3 @@ class ShoppingCartUser(HttpUser):
     def product_detail(self):
         # Test loading time of a specific product page (product 47)
         self.client.get("/product/47")
-
-
-
-    
